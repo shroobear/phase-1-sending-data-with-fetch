5ab7908ff3fee38dd718cf659615deb28653635b
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-04-23 21:32:56 -0400

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const userData = {
+      name: userName,
+      email: userEmail
+    };
+  
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then(data => {
+      const userId = data.id;
+      document.body.innerHTML += `<p>User ID: ${userId}</p>`;
+    })
+    .catch(error => {
+      document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    });
+  }
+  
